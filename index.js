a0fb3aea59cd68c554ca8bfa49a0e05e11246dea
iamvishal345 <vishalagnihotri345@gmail.com>
2021-02-28 22:17:31 +0530

    Fixed touchevent issue

--- index.js
+++ index.js
@@ -5,8 +5,14 @@ const container = document.querySelector(".container");
 
 const drag = (e) => {
   let xPos = e.layerX;
+  if (e.targetTouches) {
+    console.log(e.targetTouches[0].clientX);
+    xPos = e.targetTouches[0].clientX - 50;
+  }
+
   before.style.width = xPos + "px";
   slider.style.left = xPos + "px";
+  e.preventDefault();
 };
 
 container.addEventListener("mousemove", drag);
